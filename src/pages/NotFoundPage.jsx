66dbff867e371a387be769b45817e43d7baeb232
Francheska Nicole Aguil <faguil2024@hccci.edu.ph>
2026-09-11 12:09:45 +0800

    Task 1: install react-router

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router";
+
 const NotFoundPage = () => {
   return (
     <div class="grid min-h-screen place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
@@ -28,18 +30,18 @@ const NotFoundPage = () => {
         </p>
 
         <div class="mt-10 flex items-center justify-center gap-x-6">
-          <a
-            href="#"
+          <Link
+            to="/"
             class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
             Go back home
-          </a>
-          <a
-            href="#"
+          </Link>
+          <Link
+            to="/contact"
             class="text-sm font-semibold text-gray-900 hover:text-indigo-600"
           >
             Contact support <span aria-hidden="true">&rarr;</span>
-          </a>
+          </Link>
         </div>
       </div>
     </div>
